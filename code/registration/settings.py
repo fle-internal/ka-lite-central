ea0525c52e80832ef755f1d40fc2831fa18d4b2e
Ben Cipollini <bcipolli@ucsd.edu>
2014-03-19 09:20:36 -0700

    Add registration settings.

--- code/registration/settings.py
+++ code/registration/settings.py
@@ -1,3 +1,21 @@
 import os
 
+try:
+    import local_settings
+except ImportError:
+    local_settings = object()
+
+
+#######################
+# Set module settings
+#######################
+
+# Duplicated from contact
+CENTRAL_SERVER_DOMAIN = getattr(local_settings, "CENTRAL_SERVER_DOMAIN", "learningequality.org")
+CENTRAL_FROM_EMAIL    = getattr(local_settings, "CENTRAL_FROM_EMAIL",    "kalite@%s" % CENTRAL_SERVER_DOMAIN)
+
+# Only for registration
+ACCOUNT_ACTIVATION_DAYS = getattr(local_settings, "ACCOUNT_ACTIVATION_DAYS", 7)
+DEFAULT_FROM_EMAIL      = getattr(local_settings, "DEFAULT_FROM_EMAIL", CENTRAL_FROM_EMAIL)
+
 TEMPLATE_DIRS = (os.path.join(os.path.dirname(__file__), "templates"),)
