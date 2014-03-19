73561c3a322d3e4a7574e7510fe111ef89ee22d4
Ben Cipollini <bcipolli@ucsd.edu>
2014-03-19 05:54:00 -0700

    Adding khanload_central code.

---
+++ code/khanload_central/api_urls.py
@@ -0,0 +1,9 @@
+"""
+"""
+from django.conf.urls.defaults import patterns, include, url
+
+urlpatterns = patterns('khanload_central.api_views',
+    # Central server urls
+    url(r'^update/central/$', 'update_all_central', {}, 'update_all_central'),
+    url(r'^oauth/$', 'update_all_central_callback', {}, 'update_all_central_callback'),
+)
