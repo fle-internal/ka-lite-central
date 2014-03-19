a55137712130e46f3af5bcf447ff8531989294c2
Ben Cipollini <bcipolli@ucsd.edu>
2014-03-19 09:20:49 -0700

    Add i18n_central settings.

---
+++ code/i18n_central/settings.py
@@ -0,0 +1,19 @@
+try:
+    import local_settings
+except ImportError:
+    local_settings = object()
+
+
+#######################
+# Set module settings
+#######################
+
+CROWDIN_PROJECT_ID      = getattr(local_settings, "CROWDIN_PROJECT_ID", None)
+CROWDIN_PROJECT_KEY     = getattr(local_settings, "CROWDIN_PROJECT_KEY", None)
+
+KA_CROWDIN_PROJECT_ID      = getattr(local_settings, "KA_CROWDIN_PROJECT_ID", None)
+KA_CROWDIN_PROJECT_KEY     = getattr(local_settings, "KA_CROWDIN_PROJECT_KEY", None)
+
+AMARA_USERNAME          = getattr(local_settings, "AMARA_USERNAME", None)
+AMARA_API_KEY           = getattr(local_settings, "AMARA_API_KEY", None)
+
