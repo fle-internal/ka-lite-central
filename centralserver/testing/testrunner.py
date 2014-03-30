9fd11cfdd55838c09fc5596399a3b3ebec35529f
Ben Cipollini <bcipolli@ucsd.edu>
2014-03-30 11:23:02 -0700

    Adding test runner and test settings for central server.

---
+++ centralserver/testing/testrunner.py
@@ -0,0 +1,24 @@
+"""
+Test support harness to make setup.py test work.
+"""
+import functools
+import os
+import pdb
+import sys
+
+from django.conf import settings
+from django.core import management
+
+from kalite.testing import KALiteTestRunner
+
+
+class CentralTestRunner(KALiteTestRunner):
+    """Forces us to start in liveserver mode, and only includes relevant apps to test"""
+
+    def run_tests(self, test_labels=None, **kwargs):
+        """By default, only run relevant app tests.  If you specify... you're on your own!"""
+        if not test_labels:  # by default, come in as empty list
+            test_labels = set(['central', 'securesync'])
+
+        import pdb; pdb.set_trace()
+        return super(CentralTestRunner,self).run_tests(test_labels=test_labels, **kwargs)
