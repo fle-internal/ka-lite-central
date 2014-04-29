366978f1bd170c01b47a9940ee9ae3c9734b5cf3
Ben Cipollini <bcipolli@ucsd.edu>
2014-04-29 07:58:48 -0700

    Allow resetting values directly across apps (with no warning)

--- centralserver/stats/settings.py
+++ centralserver/stats/settings.py
@@ -22,7 +22,5 @@ INSTALLED_APPS = (
 # Set module settings
 #######################
 
-import pdb; pdb.set_trace()
-STATS_DATA_PATH = ROOT_DATA_PATH #os.path.join(os.path.dirname(__file__), "data")
-INSTALLER_BASE_URL = getattr(local_settings, 'INSTALLER_BASE_URL', 'https://www.dropbox.com/s/cy3aznnt25xv0xl/')
+STATS_DATA_PATH = ROOT_DATA_PATH #os.path.join(os.path.dirname(__file__), "data")INSTALLER_BASE_URL = getattr(local_settings, 'INSTALLER_BASE_URL', 'https://www.dropbox.com/s/cy3aznnt25xv0xl/')
 INSTALLER_BASE_URL += (INSTALLER_BASE_URL[-1] != '/' and '/') or ''  # make sure there's a trailing slash.
