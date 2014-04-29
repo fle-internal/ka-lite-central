4697f9d5590cf855bc69efab89c980a47c8948de
Ben Cipollini <bcipolli@ucsd.edu>
2014-04-29 15:21:52 -0700

    Fixing broken tests from new method for displaying username, tweak query numbers.

--- centralserver/stats/settings.py
+++ centralserver/stats/settings.py
@@ -22,5 +22,6 @@ INSTALLED_APPS = (
 # Set module settings
 #######################
 
-STATS_DATA_PATH = ROOT_DATA_PATH #os.path.join(os.path.dirname(__file__), "data")INSTALLER_BASE_URL = getattr(local_settings, 'INSTALLER_BASE_URL', 'https://www.dropbox.com/s/cy3aznnt25xv0xl/')
+STATS_DATA_PATH = ROOT_DATA_PATH #os.path.join(os.path.dirname(__file__), "data")
+INSTALLER_BASE_URL = getattr(local_settings, 'INSTALLER_BASE_URL', 'https://www.dropbox.com/s/cy3aznnt25xv0xl/')
 INSTALLER_BASE_URL += (INSTALLER_BASE_URL[-1] != '/' and '/') or ''  # make sure there's a trailing slash.
