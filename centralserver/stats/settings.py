824bd534e04eef03e07fdaac4b7afee08445bba6
Jamie Alexandre <jamalex@gmail.com>
2014-05-02 10:08:49 -0700

    Update INSTALLER_BASE_URL so it will work with the 0.12.0 release.

--- centralserver/stats/settings.py
+++ centralserver/stats/settings.py
@@ -12,5 +12,5 @@ except ImportError:
 
 STATS_DATA_PATH = ROOT_DATA_PATH #os.path.join(os.path.dirname(__file__), "data")
 
-INSTALLER_BASE_URL = getattr(local_settings, 'INSTALLER_BASE_URL', 'https://www.dropbox.com/s/cy3aznnt25xv0xl/')
-INSTALLER_BASE_URL += (INSTALLER_BASE_URL[-1] != '/' and '/') or ''  # make sure there's a trailing slash.
+INSTALLER_BASE_URL = getattr(local_settings, 'INSTALLER_BASE_URL', 'https://www.dropbox.com/s/mich3t4rsw2q6hc/')
+INSTALLER_BASE_URL = INSTALLER_BASE_URL.rstrip("/") + "/"  # make sure there's a trailing slash
