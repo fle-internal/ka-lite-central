a67fbd3864dc735fd324c6c32bb5fe4921404c0d
Aron Fyodor Asor <aronasorman@gmail.com>
2014-06-05 10:10:12 -0700

    add another test to see if we can run two distributed servers

--- centralserver/central/tests/ecosystem_tests.py
+++ centralserver/central/tests/ecosystem_tests.py
@@ -11,3 +11,13 @@ class SameVersionTests(LiveServerTestCase):
             _stdout, stderr = d1.wait()
             # the command shouldn't have printed anything to stderr
             self.assertFalse(stderr)
+
+    def test_can_instantiate_two_distributed_servers(self):
+        settings = {'CENTRAL_SERVER_HOST': self.live_server_url}
+
+        with DistributedServer(**settings) as d1, DistributedServer(**settings) as d2:
+            d1.call_command('validate')
+            d2.call_command('validate')
+
+            d1.wait()
+            d2.wait()
