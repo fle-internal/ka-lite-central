0176107ef7f2561bb3390bc15f3dc0ae70c28ba7
Aron Fyodor Asor <aronasorman@gmail.com>
2014-06-05 09:22:41 -0700

    fix the single SameVersionTests test

--- centralserver/central/tests/ecosystem_tests.py
+++ centralserver/central/tests/ecosystem_tests.py
@@ -7,7 +7,7 @@ class SameVersionTests(LiveServerTestCase):
 
     def test_can_run_on_distributed_server(self):
         with DistributedServer() as d1:
-            d1.call_command('validate', output_to_stdout=True)
+            d1.call_command('validate')
             _stdout, stderr = d1.wait()
             # the command shouldn't have printed anything to stderr
-            self.assertFalse(None)
+            self.assertFalse(stderr)
