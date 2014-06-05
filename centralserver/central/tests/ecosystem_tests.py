8ae66970380f1126cc0b5e959cdb017e793a3bad
Aron Fyodor Asor <aronasorman@gmail.com>
2014-06-05 11:03:16 -0700

    initialize a zone and org for all ecosystem tests

--- centralserver/central/tests/ecosystem_tests.py
+++ centralserver/central/tests/ecosystem_tests.py
@@ -1,9 +1,27 @@
+from django.contrib.auth.models import User
 from django.test import LiveServerTestCase
+
+from centralserver.central.models import Organization
+from securesync.tests import SecuresyncTestCase
+from securesync.devices.models import Device, Zone
 
 from .utils.distributed_server_factory import DistributedServer
 
 
-class SameVersionTests(LiveServerTestCase):
+class SameVersionTests(SecuresyncTestCase, LiveServerTestCase):
+
+    def setUp(self):
+        # TODO (aron): move this entire thing into its own mixins
+        Device.own_device = None
+        self.setUp_fake_device()
+
+        self.user = User.objects.create(username='test_user',
+                                        password='invalid_password')
+        self.test_org = Organization.objects.create(name='test_org',
+                                                    owner=self.user)
+        self.test_zone = Zone.objects.create(name='test_zone')
+        self.test_zone.organization_set.add(self.test_org)
+        self.test_zone.save()
 
     def test_can_run_on_distributed_server(self):
         with DistributedServer(CENTRAL_SERVER_HOST=self.live_server_url) as d1:
