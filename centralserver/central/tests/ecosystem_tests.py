4c5b1cf5663669bbf093c3c320f8a917394d89d9
Aron Fyodor Asor <aronasorman@gmail.com>
2014-06-05 09:18:20 -0700

    create a framework for running a distributed server with custom settings

---
+++ centralserver/central/tests/ecosystem_tests.py
@@ -0,0 +1,13 @@
+from django.test import LiveServerTestCase
+
+from .utils.distributed_server_factory import DistributedServer
+
+
+class SameVersionTests(LiveServerTestCase):
+
+    def test_can_run_on_distributed_server(self):
+        with DistributedServer() as d1:
+            d1.call_command('validate', output_to_stdout=True)
+            _stdout, stderr = d1.wait()
+            # the command shouldn't have printed anything to stderr
+            self.assertFalse(None)
---
+++ centralserver/central/tests/utils/distributed_server_factory.py
@@ -0,0 +1,94 @@
+import pathlib
+import string
+from multiprocessing import Process
+from random import choice
+
+from django.conf import settings
+from fle_utils.django_utils import call_outside_command_with_output
+
+
+class DistributedServer:
+
+    def __init__(self, dbname=None, *args, **kwargs):
+        self.distributed_dir = (pathlib.Path(settings.PROJECT_PATH).parent
+                                / 'ka-lite' / 'kalite')
+        self.manage_py_path = self.distributed_dir / 'manage.py'
+
+        # setup for custom settings for this distributed server
+        self.settings_contents = self._generate_settings(**kwargs)
+
+        uniq_name = ''.join(choice(string.ascii_lowercase) for _ in range(10))
+        self.settings_name = uniq_name
+        self.settings_path = ((self.distributed_dir / self.settings_name)
+                              .with_suffix('.py'))
+
+        self.running_process = None
+
+    def _generate_settings(self, **kwargs):
+        new_settings = '''
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME"  : ":memory:",
+        "OPTIONS": {
+            "timeout": 60,
+        },
+    }
+}
+        '''
+        old_settings_path = self.distributed_dir / 'settings.py'
+        with open(old_settings_path.as_posix()) as f:
+            old_settings = f.read()
+
+        return old_settings + new_settings
+
+    def call_command(self,
+                     commandname,
+                     output_to_stdout=True,
+                     output_to_stderr=True,
+                     **kwargs):
+        '''
+        Run a command in the context of this distributed server, customized to
+        its own settings.
+
+        commandname -- the management command to run
+        output_to_stdout -- True to output the command's stdout to the console
+                            instead of capturing to a variable
+        output_to_stderr -- True to output the command's stderr to the console
+                            instead of capturing to a variable
+        '''
+        if self.running_process:
+            raise Exception('Command {} already started.'.format(commandname))
+
+        _, _err, _ret, self.running_process = call_outside_command_with_output(
+            commandname,
+            output_to_stdout=output_to_stdout,
+            output_to_stderr=output_to_stderr,
+            settings=self.settings_name,
+            manage_py_dir=self.distributed_dir.as_posix(),
+            wait=False,
+            **kwargs
+        )
+
+        return self
+
+    def wait(self):
+        '''
+        Waits for the command run by `self.call_command` to
+        finish. Returns the error code of the process.
+        '''
+        stdout, stderr = self.running_process.communicate()
+        self.running_process = None  # so we can run other commands
+        return (stdout, stderr)
+
+    def __enter__(self):
+        # write our settings file
+        with open(self.settings_path.as_posix(), 'w') as f:
+            f.write(self.settings_contents)
+
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        # delete our settings file
+        if self.settings_path.exists():
+            self.settings_path.unlink()
