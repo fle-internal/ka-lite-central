26e8731f791c2257762a486b218b5bd5f4147220
Dylan Barth <dylan.j.barth@gmail.com>
2014-06-24 11:40:25 -0700

    Only force org creation if user was not already invited to another org.

--- centralserver/registration/views.py
+++ centralserver/registration/views.py
@@ -17,7 +17,7 @@ from django.utils.translation import ugettext as _
 
 from .backends import get_backend
 from centralserver.central.forms import OrganizationForm
-from centralserver.central.models import Organization
+from centralserver.central.models import Organization, OrganizationInvitation
 from centralserver.contact.views import contact_subscribe
 from fle_utils.internet import set_query_params
 from securesync.models import Zone
@@ -207,34 +207,39 @@ def register(request, backend, success_url=None, form_class=None,
 
     do_subscribe = request.REQUEST.get("email_subscribe") == "on"
 
+    # if they've been invited, don't force org creation
+    invited_email = request.REQUEST.get("email_invite") 
     if request.method == 'POST':
         form = form_class(data=request.POST, files=request.FILES)
-        org_form = OrganizationForm(data=request.POST, instance=Organization())
 
         # Could register
-        if form.is_valid() and org_form.is_valid():
+        if form.is_valid():
             assert form.cleaned_data.get("username") == form.cleaned_data.get("email"), "Should be set equal in the call to clean()"
 
             try:
                 # Create the user
                 new_user = backend.register(request, **form.cleaned_data)
 
-                # Add an org.  Must create org before adding user.
-                org_form.instance.owner = new_user
-                org_form.save()
-                org = org_form.instance
-                org.add_member(new_user)
+                # only force add org if user was not invited
+                if not invited_email:
+                    org_form = OrganizationForm(data=request.POST, instance=Organization())
+                    if org_form.is_valid():
+                        # Add an org.  Must create org before adding user.
+                        org_form.instance.owner = new_user
+                        org_form.save()
+                        org = org_form.instance
+                        org.add_member(new_user)
 
-                # Now add a zone, and link to the org
-                zone = Zone(name=org_form.instance.name + " Sharing Network")
-                zone.save()
-                org.add_zone(zone)
+                        # Now add a zone, and link to the org
+                        zone = Zone(name=org_form.instance.name + " Sharing Network")
+                        zone.save()
+                        org.add_zone(zone)
+                        org.save()
 
                 # Finally, try and subscribe the user to the mailing list
                 # (silently; don't return anything to the user)
                 if do_subscribe:
                     contact_subscribe(request, form.cleaned_data['email'])  # no "return"
-                org.save()
 
                 if success_url is None:
                     to, args, kwargs = backend.post_registration_redirect(request, new_user)
@@ -247,11 +252,18 @@ def register(request, backend, success_url=None, form_class=None,
                     form._errors['__all__'] = _("An account with this email address has already been created.  Please login at the link above.")
                 else:
                     raise e
+        else:
+            if invited_email:
+                org_form = None
 
     # GET, not POST
     else:
-        form = form_class()
-        org_form = OrganizationForm()
+        if invited_email:
+            form = form_class(initial={'email': invited_email})
+            org_form = None
+        else:
+            form = form_class()
+            org_form = OrganizationForm()
 
     if extra_context is None:
         extra_context = {}
