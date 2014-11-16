b403aea359c0525a73ed1df2a11ae36a7d5b7635
Cyril Pauya <cpauya@gmail.com>
2014-11-17 07:47:59 +0800

    Wrap code in exception handler to capture IOErrors when opening the files.

--- centralserver/i18n/views.py
+++ centralserver/i18n/views.py
@@ -3,7 +3,9 @@
 import json
 from collections import OrderedDict
 
+from django.conf import settings
 from django.core.urlresolvers import reverse, NoReverseMatch
+from django.shortcuts import Http404
 
 from annoying.decorators import render_to
 
@@ -11,6 +13,9 @@ from kalite.version import SHORTVERSION
 
 from fle_utils.general import sort_version_list
 from . import get_language_pack_availability_filepath
+
+
+logging = settings.LOG
 
 
 @render_to('i18n/language_dashboard.html')
@@ -23,28 +28,31 @@ def language_dashboard(request):
                 '0.12.0': {
                     meta data here....
                 }
-            ] 
+            ]
         }
     """
-    with open(get_language_pack_availability_filepath()) as f:
-        lang_availability = json.load(f)
-    ordered_versions = sort_version_list([pack["software_version"] for code, pack in lang_availability.items()], reverse=True)
-    lang_pack_by_version = OrderedDict((version, []) for version in ordered_versions)
+    try:
+        with open(get_language_pack_availability_filepath()) as f:
+            lang_availability = json.load(f)
+        ordered_versions = sort_version_list([pack["software_version"] for code, pack in lang_availability.items()], reverse=True)
+        lang_pack_by_version = OrderedDict((version, []) for version in ordered_versions)
 
-    for code, pack in lang_availability.items():
-        # add a url to download the language pack
-        language_code = pack["code"]
-        try:
-            url = reverse("download_language_pack", kwargs={"version": SHORTVERSION, "lang_code": language_code})
-        except NoReverseMatch:
-            url = ""
-        pack["download_language_url"] = url
+        for code, pack in lang_availability.items():
+            # add a url to download the language pack
+            language_code = pack["code"]
+            try:
+                url = reverse("download_language_pack", kwargs={"version": SHORTVERSION, "lang_code": language_code})
+            except NoReverseMatch:
+                url = ""
+            pack["download_language_url"] = url
 
-        software_version = pack["software_version"]
-        del pack["software_version"]  # won't be used other than to sort
-        lang_pack_by_version[software_version].append(pack)
-    context = {
-        "lang_pack_by_version": lang_pack_by_version,
-        "crowdin_base_url": "https://crowdin.com/project/ka-lite/"
-    }
+            software_version = pack["software_version"]
+            del pack["software_version"]  # won't be used other than to sort
+            lang_pack_by_version[software_version].append(pack)
+        context = {
+            "lang_pack_by_version": lang_pack_by_version,
+            "crowdin_base_url": "https://crowdin.com/project/ka-lite/"
+        }
+    except Exception:
+        raise Http404
     return context
