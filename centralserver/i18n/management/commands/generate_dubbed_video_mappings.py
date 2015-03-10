29d1de287d0760288f2c02a33b6a250c6f6b2f49
Aron Fyodor Asor <aronasorman@gmail.com>
2015-03-10 10:09:41 -0700

    update generate_dubbed_video_mapping to the latest KA spreadsheet format

--- centralserver/i18n/management/commands/generate_dubbed_video_mappings.py
+++ centralserver/i18n/management/commands/generate_dubbed_video_mappings.py
@@ -83,7 +83,7 @@ def generate_dubbed_video_mappings_from_csv(csv_data=None):
     for row in reader:
 
         # skip over the header rows
-        if row[0].strip() in ["", "UPDATED:"]:
+        if row[0].strip() in ["", "LAST UPDATED"]:
             continue
 
         elif row[0] == "SERIAL":
