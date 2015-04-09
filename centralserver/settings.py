7cc827822c74bd2bcc2a2e4fe77f0e2281411da1
Michael Gallaspy <gallaspy.michael@gmail.com>
2015-04-09 12:31:21 -0700

    Remove cruft, add box url

--- centralserver/settings.py
+++ centralserver/settings.py
@@ -49,16 +49,6 @@ KALITE_PATH    = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..',
 LOCALE_PATHS   = getattr(local_settings, "LOCALE_PATHS", (PROJECT_PATH + "/../locale",))
 LOCALE_PATHS   = tuple([os.path.realpath(lp) + "/" for lp in LOCALE_PATHS])
 
-#DATABASES      = getattr(local_settings, "DATABASES", {
-#    "default": {
-#        "ENGINE": getattr(local_settings, "DATABASE_TYPE", "django.db.backends.sqlite3"),
-#        "NAME"  : getattr(local_settings, "DATABASE_PATH", os.path.join(PROJECT_PATH, "database", "data.sqlite")),
-#        "OPTIONS": {
-#            "timeout": 60,
-#        },
-#    }
-#})
-
 # You'll probably want to change this for production.
 DATABASES      = {
     "default": {
