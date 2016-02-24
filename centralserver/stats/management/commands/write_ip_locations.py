ee07e99ab2cc0a1e92383db1745465f4a965f35e
Jamie Alexandre <jamalex@gmail.com>
2016-02-23 20:41:52 -0800

    Updates to code to extract location information from IP addresses.

--- centralserver/stats/management/commands/write_ip_locations.py
+++ centralserver/stats/management/commands/write_ip_locations.py
@@ -7,14 +7,19 @@ import pygeoip
 import re
 import simplekml
 
+
 from optparse import make_option
 
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
+from django.core.paginator import Paginator
 
 from securesync.models import SyncSession
 
 GEOIPDAT = os.path.join(settings.ROOT_DATA_PATH, "GeoLiteCity.dat")
+
+COUNTRIES = set(["Andorra","United Arab Emirates","Afghanistan","Antigua and Barbuda","Albania","Armenia","Angola","Argentina","Austria","Australia","Azerbaijan","Bosnia and Herzegovina","Barbados","Bangladesh","Belgium","Burkina Faso","Bulgaria","Bahrain","Burundi","Benin","Brunei Darussalam","Bolivia","Brazil","Bahamas","Bhutan","Botswana","Belarus","Belize","Canada","Congo, The Democratic Republic of the","Central African Republic","Congo","Switzerland","Cote D'Ivoire","Chile","Cameroon","China","Colombia","Costa Rica","Cuba","Cape Verde","Cyprus","Czech Republic","Germany","Djibouti","Denmark","Dominica","Dominican Republic","Algeria","Ecuador","Estonia","Egypt","Eritrea","Spain","Ethiopia","Finland","Fiji","Micronesia, Federated States of","France","Gabon","United Kingdom","Grenada","Georgia","Ghana","Gambia","Guinea","Equatorial Guinea","Greece","Guatemala","Guinea-Bissau","Guyana","Honduras","Croatia","Haiti","Hungary","Indonesia","Ireland","Israel","India","Iraq","Iran, Islamic Republic of","Iceland","Italy","Jamaica","Jordan","Japan","Kenya","Kyrgyzstan","Cambodia","Kiribati","Comoros","Saint Kitts and Nevis","Korea, Democratic People's Republic of","Korea, Republic of","Kuwait","Kazakhstan","Lao People's Democratic Republic","Lebanon","Saint Lucia","Liechtenstein","Sri Lanka","Liberia","Lesotho","Lithuania","Luxembourg","Latvia","Libya","Morocco","Monaco","Moldova, Republic of","Montenegro","Madagascar","Marshall Islands","Macedonia","Mali","Myanmar","Mongolia","Mauritania","Malta","Mauritius","Maldives","Malawi","Mexico","Malaysia","Mozambique","Namibia","Niger","Nigeria","Nicaragua","Netherlands","Norway","Nepal","Nauru","New Zealand","Oman","Panama","Peru","Papua New Guinea","Philippines","Pakistan","Poland","Portugal","Palau","Paraguay","Qatar","Romania","Serbia","Russian Federation","Rwanda","Saudi Arabia","Solomon Islands","Seychelles","Sudan","Sweden","Singapore","Slovenia","Slovakia","Sierra Leone","San Marino","Senegal","Somalia","Suriname","South Sudan","Sao Tome and Principe","El Salvador","Syrian Arab Republic","Swaziland","Chad","Togo","Thailand","Tajikistan","Timor-Leste","Turkmenistan","Tunisia","Tonga","Turkey","Trinidad and Tobago","Tuvalu","Taiwan","Tanzania, United Republic of","Ukraine","Uganda","United States","Uruguay","Uzbekistan","Holy See (Vatican City State)","Saint Vincent and the Grenadines","Venezuela","Vietnam","Vanuatu","Samoa","Yemen","South Africa","Zambia","Zimbabwe"])
+TERRITORIES = set(["Anguilla","Antarctica","American Samoa","Aruba","Aland Islands","Saint Bartelemey","Bermuda","Bonaire, Saint Eustatius and Saba","Bouvet Island","Cocos (Keeling) Islands","Cook Islands","Curacao","Christmas Island","Western Sahara","Falkland Islands (Malvinas)","Faroe Islands","French Guiana","Guernsey","Gibraltar","Greenland","Guadeloupe","South Georgia and the South Sandwich Islands","Guam","Hong Kong","Heard Island and McDonald Islands","Isle of Man","British Indian Ocean Territory","Jersey","Cayman Islands","Saint Martin","Macao","Northern Mariana Islands","Martinique","Montserrat","New Caledonia","Norfolk Island","Niue","French Polynesia","Saint Pierre and Miquelon","Pitcairn","Puerto Rico","Palestinian Territory","Reunion","Saint Helena","Svalbard and Jan Mayen","Sint Maarten","French Southern Territories","Tokelau","Turks and Caicos Islands","United States Minor Outlying Islands","Virgin Islands, British","Virgin Islands, U.S.","Wallis and Futuna","Mayotte","Macau"])
 
 class Command(BaseCommand):
     help = "Generate IP address list."
@@ -74,20 +79,24 @@ class Command(BaseCommand):
 
         ip_metadata = {}
 
-        for session in SyncSession.objects.order_by("timestamp"):
-            for addr in session.ip.split(","):
-                addr = addr.strip()
+        # We paginate to keep memory usage low while loading large chunks of the queryset at once
+        pages = Paginator(SyncSession.objects.order_by("timestamp").values("ip", "client_device_id"), 50000)
+        for page in range(1, pages.num_pages + 1):
+            print "Page", page, "of", pages.num_pages
+            for session in pages.page(page).object_list:
+                for addr in session["ip"].split(","):
+                    addr = addr.strip()
 
-                if addr not in BLACKLIST:
+                    if addr not in BLACKLIST:
 
-                    # update the ip_metadata
-                    ip_metadata[addr] = ip_metadata.get(addr, {"devices": set([]), "location": None})
-                    ip_metadata[addr]["devices"].add(session.client_device.id)
+                        # update the ip_metadata
+                        ip_metadata[addr] = ip_metadata.get(addr, {"devices": set([]), "location": None})
+                        ip_metadata[addr]["devices"].add(session["client_device_id"])
 
-                    # update the list of ips
-                    if addr not in ip_set:
-                        ips.append(addr)
-                        ip_set.add(addr)
+                        # update the list of ips
+                        if addr not in ip_set:
+                            ips.append(addr)
+                            ip_set.add(addr)
 
         locations, all_locations = ips_to_locations(ips)
 
@@ -123,13 +132,20 @@ class Command(BaseCommand):
         if options.get("country_file"):
             with open(options["country_file"], "w") as f:
                 self.stdout.write("Writing list of countries as JSONP data to %s!\n" % options["country_file"])
-                countries = get_countries(locations)
-                jsonp = "display_countries(%s);" % json.dumps(countries)
+                countries, territories = get_countries_and_territories(locations)
+                jsonp = "display_countries(%s); display_territories(%s);" % (json.dumps(list(countries)), json.dumps(list(territories)))
                 f.write(jsonp)
                 states = get_states(locations)
-                print "Countries (%d):" % len(countries)
-                # print "\n".join(sorted(country.replace("United States", "U.S.A. (%d+ states)" % len(states)) for country in countries))
+                missing_countries, missing_territories = get_missing_countries_and_territories(locations)
+                print "\nCountries (%d):" % len(countries)
                 print "\n".join(sorted(country for country in countries))
+                print "\nTerritories (%d):" % len(territories)
+                print "\n".join(sorted(territory for territory in territories))
+                print ""
+                print "\nMissing countries (%d):" % len(missing_countries)
+                print "\n".join(sorted(country for country in missing_countries))
+                print "\nMissing territories (%d):" % len(missing_territories)
+                print "\n".join(sorted(territory for territory in missing_territories))
 
         if options.get("country_csv"):
             with open(options["country_csv"], "w") as f:
@@ -169,9 +185,6 @@ def ips_to_locations(ips):
     return locations, all_locations
 
 
-COUNTRY_STRINGS_TO_REMOVE = [", Republic of", ", United Republic of", ", Islamic Republic of"]
-
-
 def count_devices_by_region(ip_metadata, region_field):
     regions = {}
     for ip, data in ip_metadata.items():
@@ -183,17 +196,21 @@ def count_devices_by_region(ip_metadata, region_field):
         regions[region] = len(regions[region])
     return regions
 
-def get_countries(locations, continent=None):
-    countries = set([])
+def get_countries_and_territories(locations, continent=None):
+    items = set([])
     for record in locations:
         if record:
             if continent and continent != record["continent"]:
                 continue
-            country_name = record['country_name']
-            for substring in COUNTRY_STRINGS_TO_REMOVE:
-                country_name = country_name.replace(substring, "")
-            countries.add(country_name)
-    return list(countries.union(["Bhutan", "Central African Republic"]) - set(['Anonymous Proxy', 'Satellite Provider', 'Asia/Pacific Region', 'Virgin Islands, U.S.']))
+            item_name = record['country_name']
+            items.add(item_name)
+    items.add("Bhutan") # We know it!
+    print "MISSING", items - COUNTRIES.union(TERRITORIES)
+    return items.intersection(COUNTRIES), items.intersection(TERRITORIES)
+
+def get_missing_countries_and_territories(locations, continent=None):
+    countries, territories = get_countries_and_territories(locations, continent=continent)
+    return COUNTRIES - countries, TERRITORIES - territories
 
 def get_states(locations, country=["United States", "U.S.A."]):
     if isinstance(country, basestring):
