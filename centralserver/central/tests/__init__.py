0cd58238b43322572df4bcc8f7716168515a39ef
Benjamin Bach <benjamin@overtag.dk>
2019-09-12 17:45:45 +0200

    Remove empty test module

--- centralserver/central/tests/__init__.py
+++ centralserver/central/tests/__init__.py
@@ -3,6 +3,5 @@ from .browser_tests import *
 from .ecosystem_tests import *
 from .fixture_tests import *
 from .invitation_tests import *
-from .pot_tests import *
 from .unicode_tests import *
 from .url_tests import *
