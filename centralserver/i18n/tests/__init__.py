33d17787e454620c8faba2f4d37fce42385014ec
Benjamin Bach <benjamin@overtag.dk>
2019-09-12 16:08:23 +0200

    Comment about a missing test module

--- centralserver/i18n/tests/__init__.py
+++ centralserver/i18n/tests/__init__.py
@@ -1,1 +1,5 @@
-from .translation_tests import *
+# This module was removed, it only really tested that there weren't
+# untranslated stuff and format strings - such errors can be spotted
+# in other ways
+# See: https://github.com/fle-internal/ka-lite-central/blob/057443c942794c62b2bdf1f5f95a270457414ac2/centralserver/i18n/tests/translation_tests.py
+# from .translation_tests import *
